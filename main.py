1144d928ee77c9e25524c6d4fd94ce47cd372e4f
Angel Moreno <113930724+Amoreno73@users.noreply.github.com>
2022-09-19 17:14:36 +0000

    I added notes on different ways of changing values on a list

--- main.py
+++ main.py
@@ -16,7 +16,18 @@ print(newSentence.rindex("practice"))
 ###############################################Review############################################################
 #####################################challenge 1################################################################
 # Create a list with 5 elements, inside the variable my_list. You can include strings, booleans, numbers, etc.
-my_list = ["bay","ddaa", 34, 56]
+my_list = ["bay","ddaa", 34, 56, 78, 89, 102]
+# append = adds to the end of a list
+my_list.append("jewelry")
+print(my_list)
+# pop() removes from the end
+my_list.pop()
+print(my_list)
+# remove removes an item at a certain index
+my_list.remove("ddaa")
+print(my_list)
+my_list.insert(3, "bobba bobba")
+print(my_list)
 
 
 # Add the element "motorcycle" to the following list of means of transportation:
